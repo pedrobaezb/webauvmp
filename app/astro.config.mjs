2593e2f957dfede91c56c786e39b21e913d6855e
Pedro Baez <pedrobaezb@gmail.com>
2023-02-08 23:33:55 +0100

    Corregida la configuración de Astro para que coincida con donde esta alojada la demo actualmente

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -8,5 +8,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  site: "https://auvmp.org/",
+  base: "/nueva-version/"
 });
