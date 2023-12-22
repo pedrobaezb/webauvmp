32e28f11e606b0686ca58263c8dfd784c0762c05
Pedro Baez <pedrobaezb@gmail.com>
2023-12-22 01:26:25 +0100

    Areeglado para poder desplegar correctamente en kubernetes

--- app/buildimage.cmd
+++ app/buildimage.cmd
@@ -1,2 +1,2 @@
- docker build --platform linux/arm64 -t 192.168.1.10:32000/web-auvmp-frontend:23.07.19 .
- docker push 192.168.1.10:32000/web-auvmp-frontend:23.07.19
+ docker build --platform linux/arm64 -t 192.168.1.10:32000/web-auvmp-frontend:23.12.21 .
+ docker push 192.168.1.10:32000/web-auvmp-frontend:23.12.21
